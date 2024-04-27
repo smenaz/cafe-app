afa5544eb5233d4f8e1fac298285c8a988eee5c0
Sergio Mena <148154225+smenaz@users.noreply.github.com>
2024-04-26 21:57:21 -0600

    Se agrego el servicio y se actualizo angular para el proyecto.
    Se agrego el servicio y se actualizo angular para el proyecto. Se agrego enviroment para dev.

---
+++ src/app/cafe/cafe.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cafe } from './cafe';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CafeService {
+
+  private apiUrl: string = environment.baseUrl
+
+  constructor(private http: HttpClient) { }
+
+  get_cafes_avaiables(): Observable<Cafe[]> {
+    return this.http.get<Cafe[]>(this.apiUrl);
+  }
+
+
+}
